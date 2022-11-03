3e95b1bffdb71ff08cfb64047f98116b17ea20c3
iisyos <kosuke1216a@yahoo.co.jp>
2022-11-03 22:15:18 +0900

    v3.1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          v3.0
+          v3.1
         </p>
         <a
           className="App-link"
